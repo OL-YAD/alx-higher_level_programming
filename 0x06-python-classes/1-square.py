220ac0803fb498e1e7c083fff3f127fa4944d2b1
OL-YAD <olyadtemesgen@gmail.com>
2022-06-21 18:15:00 +0300

    private instance

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+"""  oop with python """
+
+
+class Square:
+    """Class representing a square"""
+
+    def __init__(self, size):
+        """ initialize the class
+        Args:
+           size (int): size of the square
+        """
+        self.__size = size
