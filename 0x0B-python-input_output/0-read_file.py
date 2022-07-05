73168d48b24e9de4c1c37a92ea9c84c07fd2d219
OL-YAD <olyadtemesgen@gmail.com>
2022-07-05 16:09:59 +0300

    read text file using with

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,13 +1,11 @@
-!/usr/bin/python3
-""" Module that contains a function that reads from a file """
+#!/usr/bin/python3
+'''
+Module to read and print contents of a file
+'''
 
 
 def read_file(filename=""):
-    """ Function that reads from a file
-    Args:
-        filename: filename
-    """
-
-    with open(filename, 'r', encoding="utf-8") as f:
-        read_data = f.read()
-        print(read_data, end='')
+    ''' Reads file and prints contents '''
+    with open(filename) as open_file:
+        contents = open_file.read()
+    print(contents, end="")
