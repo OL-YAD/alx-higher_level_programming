568c7e2457d7b5e25f2ac18b2fdf77c0bfa44f4b
OL-YAD <olyadtemesgen@gmail.com>
2022-08-09 01:24:01 +0300

    sql basic

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,4 @@
+-- create DB.
+-- if the database already exists , the script should not fail
+
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0
---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,6 @@
+-- a script that creates a table in the current database
+
+CREATE TABLE IF NOT EXISTS first_table(
+    id INT,
+    name VARCHAR(256)
+);
