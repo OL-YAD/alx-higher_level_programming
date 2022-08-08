ad8de9c859b116b11e94188442faa8a9c2ebe3f8
OL-YAD <olyadtemesgen@gmail.com>
2022-08-09 02:36:12 +0300

    sql basic

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,6 +1,12 @@
 -- create DB with their records 
 
-CREATE TABLE IF NOT EXISTS second_table(id INT, name VARCHAR(256),score INT);
+CREATE TABLE IF NOT EXISTS second_table(
+    id INT, 
+    name VARCHAR(256), 
+    score INT);
+
 INSERT INTO second_table(id, name, score) 
-VALUES(1, "JOHN", 10),(2,"ALEX",3),(3, "BOb", 14),('4', "George", "8");
+VALUES(1, JOHN, 10),(2,ALEX,3),(3, BOb, 14),(4, George, 8);
+
+
 
