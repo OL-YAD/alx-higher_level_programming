dc80509c04c3c0125dd04ab57be418848740b1d4
OL-YAD <olyadtemesgen@gmail.com>
2022-08-10 15:22:21 +0300

    create user

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,8 +1,7 @@
--- a script that creates the MySQL server user user_0d_1
+-- a script that creates the MySQL server user user_0d_1 and grant all privileges 
 
 CREATE USER IF NOT EXISTS user_0d_1@localhost
 IDENTIFIED BY user_0d_1_pwd;
 
--- grant all privileges
 GRANT ALL PRIVILEGES ON *.* TO user_0d_1@localhost;
 
