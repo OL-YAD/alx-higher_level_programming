6454895168423f50397746439c1e05da3a33f8c7
OL-YAD <olyadtemesgen@gmail.com>
2022-08-11 15:12:40 +0300

    sql

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -11,4 +11,4 @@ FROM tv_genres
 INNER JOIN tv_show_genres 
 ON tv_genres.id = tv_show_genres.genre_id
 GROUP BY tv_show_genres.genre_id
-GROUP BY number_of_shows DESC;
+ORDER BY number_of_shows DESC;
